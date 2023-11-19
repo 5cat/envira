a7d3b9c6fb24d44e9f102a81aae9301b17d6913a
5cat <cat@heaven.ist>
2023-11-20 04:07:41 +0800

    improving the API and adding unit tests

--- src/envira/__init__.py
+++ src/envira/__init__.py
@@ -1,29 +1,41 @@
-from abc import ABC, abstractmethod
 import inspect
-from typing import  Optional, Union, get_args, get_origin
+from typing import ClassVar, Literal, Optional, Union, get_args, get_origin, Callable, Any
 import os
 
-class Handler(ABC):
-    def __init__(self):
-        pass
+from datetime import datetime
 
-    @abstractmethod
-    def convert(self, value, annot, env: 'Env'):
-        pass
 
-    def detect(self, value, annot, env: 'Env') -> bool:
-        return True
+def is_type(type_: type, annot, _) -> bool:
+    if len(get_args(annot)) > 0:
+        annot_type = get_origin(annot)
+    else:
+        annot_type = annot
+    return annot_type is type_
 
 
 class Env:
-    def __init__(self):
-        annotations = inspect.get_annotations(type(self))
-        self.handlers = self._get_all_handlers()
+    _handlers: ClassVar[dict[type, Callable]] = {}
+    _handlers_detectors: ClassVar[dict[type, Callable[[type, Any, Any], bool]]] = {}
 
+    def __init__(self):
+        annotations = {}
+        for class_type in type(self).__mro__:
+            if not issubclass(class_type, Env):
+                break
+            annotations.update(inspect.get_annotations(class_type))
         for var, annot in annotations.items():
+            if get_origin(annot) is ClassVar:
+                continue
             value = os.environ.get(var)
-            value = self.handle(value, annot)
-            setattr(self, var, value)
+            if getattr(self, var, None) is not None and value is None:
+                pass
+            else:
+                value = self.handle(value, annot)
+                setattr(self, var, value)
+
+    def __init_subclass__(cls) -> None:
+        cls._handlers = cls._handlers.copy()
+        cls._handlers_detectors = cls._handlers_detectors.copy()
 
     def __repr__(self) -> str:
         annotations = inspect.get_annotations(type(self))
@@ -34,76 +46,88 @@ class Env:
         return self.__repr__()
 
     def handle(self, value, annot):
-        for handler in self.handlers:
-            if handler.detect(value, annot, self):
-                value = handler.convert(value, annot, self)
-                break
+        for type_, detector in self._handlers_detectors.items():
+            if detector(type_, annot, value):
+                return self._handlers[type_](value, annot, self)
         else:
-            value = annot(value)
-        return value
+            return annot(value)
+
+    @classmethod
+    def add_handler(
+        cls, type_: type, detector: Callable[[type, Any, Any], bool] = is_type
+    ):
+        def decorator(handler: Callable) -> Callable:
+            cls._handlers[type_] = handler
+            cls._handlers_detectors[type_] = detector
+            return handler
+
+        return decorator
 
 
-    def _get_all_handlers(self):
-        handlers: list[Handler] = []
-        for attribute in dir(self):
-            if attribute in ['_get_all_handlers']:
-                continue
-            obj = getattr(self, attribute)
-            try:
-                is_handler = issubclass(obj, Handler)
-            except TypeError:
-                is_handler = False
-            if is_handler:
-                handlers.append(obj())
+@Env.add_handler(Union)
+def union_handler(value: Optional[str], annot, env):
+    if value is None or value.lower() in ["none", "null"]:
+        return None
 
-        return handlers
-
-    class OptionalHandler(Handler):
-        def convert(self, value: Optional[str], annot, env):
-            if value is None or value.lower() == "none":
-                return None
-
-            type_, _ = get_args(annot)
-            return env.handle(value, type_)
-
-        def detect(self, value, annot, env) -> bool:
-            if get_origin(annot) is Union:
-                return type(None) in get_args(annot)
-
-            return False
-
-    class ListHandler(Handler):
-        def convert(self, value: str, annot, env):
-            values = []
-            type_ = get_args(annot)[0]
-            for v in value.split(";"):
-                values.append(env.handle(v, type_))
-            return values
-
-        def detect(self, value, annot, env: 'Env') -> bool:
-            return isinstance(value, str) and get_origin(annot) is list
-
-    class DictHandler(Handler):
-        def convert(self, value: str, annot, env):
-            values = {}
-            key_type, value_type = get_args(annot)
-            for entry in value.split(","):
-                k, v = entry.split("=")
-                k = env.handle(k, key_type)
-                v = env.handle(v, value_type)
-                values[k] = v
-            return values
-
-        def detect(self, value, annot, env: 'Env') -> bool:
-            return isinstance(value, str) and get_origin(annot) is dict
+    type_, _ = get_args(annot)
+    return env.handle(value, type_)
 
 
-class MyEnv(Env):
-    TEST_A: Optional[int]
-    TEST_B: int
-    TEST_C: list[int]
-    TEST_D: dict[str, int]
+@Env.add_handler(list)
+def list_handler(value: str, annot, env):
+    values = []
+    annot_args = get_args(annot)
+    if len(annot_args) == 0:
+        type_ = str
+    else:
+        type_ = annot_args[0]
+    for v in value.split(";"):
+        values.append(env.handle(v, type_))
+    return values
 
 
+@Env.add_handler(dict)
+def dict_handler(value: str, annot, env):
+    values = {}
+    annot_args = get_args(annot)
+    if len(annot_args) == 0:
+        key_type, value_type = str, str
+    else:
+        key_type, value_type = annot_args
+    for entry in value.split(","):
+        k, v = entry.split("=")
+        k = env.handle(k, key_type)
+        v = env.handle(v, value_type)
+        values[k] = v
+    return values
 
-print(MyEnv())
+
+@Env.add_handler(datetime)
+def datetime_handler(value: str, _, __):
+    if len(set(value) - set('0123456789.')) == 0:
+        return datetime.fromtimestamp(float(value))
+    else:
+        return datetime.fromisoformat(value)
+
+
+@Env.add_handler(bool)
+def bool_handler(value: Optional[str], _, __):
+    if value is None:
+        return False
+    if value.lower() in ["true", "yes", "1"]:
+        return True
+    if value.lower() in ["false", "no", "0"]:
+        return False
+    raise ValueError("could not parse bool")
+
+
+@Env.add_handler(Literal)
+def literal_handler(value: str, annot, env: 'Env'):
+    for allowed_value in get_args(annot):
+        try:
+            new_value = env.handle(value, type(allowed_value))
+            if new_value == allowed_value:
+                return new_value
+        except ValueError:
+            pass
+    raise ValueError("value is not in the allowed values")
---
+++ tests/test_env.py
@@ -0,0 +1,225 @@
+from typing import Literal, Optional
+from envira import Env
+
+from enum import Enum
+from datetime import datetime
+
+
+def test_int(monkeypatch):
+    monkeypatch.setenv("A", "123")
+
+    class MyEnv(Env):
+        A: int
+
+    my_env = MyEnv()
+
+    assert my_env.A == 123 and isinstance(my_env.A, int)
+
+
+def test_float(monkeypatch):
+    monkeypatch.setenv("A", "23.01")
+
+    class MyEnv(Env):
+        A: float
+
+    my_env = MyEnv()
+
+    assert my_env.A == 23.01 and isinstance(my_env.A, float)
+
+
+def test_str(monkeypatch):
+    monkeypatch.setenv("A", "hohoho")
+
+    class MyEnv(Env):
+        A: str
+
+    my_env = MyEnv()
+
+    assert my_env.A == "hohoho"
+
+
+def test_optional_none():
+    class MyEnv(Env):
+        A: Optional[str]
+
+    my_env = MyEnv()
+
+    assert my_env.A is None
+
+
+def test_optional(monkeypatch):
+    monkeypatch.setenv("A", "aa")
+
+    class MyEnv(Env):
+        A: Optional[str]
+
+    my_env = MyEnv()
+
+    assert my_env.A == "aa"
+
+
+def test_list(monkeypatch):
+    monkeypatch.setenv("A", "1;a;3")
+
+    class MyEnv(Env):
+        A: list
+
+    my_env = MyEnv()
+
+    assert my_env.A == ["1", "a", "3"]
+
+
+def test_list_int(monkeypatch):
+    monkeypatch.setenv("A", "1;2;3")
+
+    class MyEnv(Env):
+        A: list[int]
+
+    my_env = MyEnv()
+
+    assert my_env.A == [1, 2, 3]
+
+
+def test_dict(monkeypatch):
+    monkeypatch.setenv("A", "a=e,b=c")
+
+    class MyEnv(Env):
+        A: dict
+
+    my_env = MyEnv()
+
+    assert my_env.A == {"a": "e", "b": "c"}
+
+
+def test_dict_int_float(monkeypatch):
+    monkeypatch.setenv("A", "1=32.5,7=5.63")
+
+    class MyEnv(Env):
+        A: dict[int, float]
+
+    my_env = MyEnv()
+
+    assert my_env.A == {1: 32.5, 7: 5.63}
+
+
+def test_int_default():
+    class MyEnv(Env):
+        A: int = 654
+
+    my_env = MyEnv()
+
+    assert my_env.A == 654
+
+
+def test_optional_int_default():
+    class MyEnv(Env):
+        A: Optional[int] = None
+
+    my_env = MyEnv()
+
+    assert my_env.A is None
+
+
+def test_enum(monkeypatch):
+    monkeypatch.setenv("A", "0")
+    monkeypatch.setenv("B", "1")
+
+    class MyEnum(Enum):
+        X = "0"
+        Y = "1"
+
+    class MyEnv(Env):
+        A: MyEnum
+        B: MyEnum
+
+    my_env = MyEnv()
+    assert my_env.A is MyEnum.X
+    assert my_env.B is MyEnum.Y
+
+
+def test_datetime_iso(monkeypatch):
+    datetime_value = "2023-11-19T19:43:23.023535"
+    monkeypatch.setenv("A", datetime_value)
+
+    class MyEnv(Env):
+        A: datetime
+
+    my_env = MyEnv()
+    assert my_env.A == datetime.fromisoformat(datetime_value)
+
+
+def test_datetime_timestamp(monkeypatch):
+    datetime_value = "1700394203.023535"
+    monkeypatch.setenv("A", datetime_value)
+
+    class MyEnv(Env):
+        A: datetime
+
+    my_env = MyEnv()
+    assert my_env.A == datetime.fromtimestamp(float(datetime_value))
+
+
+def test_bool_true(monkeypatch):
+    monkeypatch.setenv("A", "TRUE")
+    monkeypatch.setenv("B", "true")
+    monkeypatch.setenv("C", "True")
+
+    class MyEnv(Env):
+        A: bool
+        B: bool
+        C: bool
+
+    my_env = MyEnv()
+    assert my_env.A == True
+    assert my_env.B == True
+    assert my_env.C == True
+
+
+def test_bool_false(monkeypatch):
+    monkeypatch.setenv("A", "FALSE")
+    monkeypatch.setenv("B", "false")
+    monkeypatch.setenv("C", "False")
+
+    class MyEnv(Env):
+        A: bool
+        B: bool
+        C: bool
+
+    my_env = MyEnv()
+    assert my_env.A == False
+    assert my_env.B == False
+    assert my_env.C == False
+
+
+def test_bool_empty():
+    class MyEnv(Env):
+        A: bool
+
+    my_env = MyEnv()
+    assert my_env.A == False
+
+
+def test_literal(monkeypatch):
+    monkeypatch.setenv("A", "DEBUG")
+    monkeypatch.setenv("B", "1")
+
+    class MyEnv(Env):
+        A: Literal['DEBUG', 'INFO', 'WARN', 'ERROR']
+        B: Literal[0, 1]
+        C: Literal[False, 's']
+
+    my_env = MyEnv()
+
+    assert my_env.A == 'DEBUG'
+    assert my_env.B == 1
+    assert my_env.C == False
+
+    monkeypatch.setenv("A", "WARN")
+    monkeypatch.setenv("B", "0")
+    monkeypatch.setenv("C", "s")
+
+    my_env = MyEnv()
+
+    assert my_env.A == 'WARN'
+    assert my_env.B == 0
+    assert my_env.C == 's'
